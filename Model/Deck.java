0776ea01d3187ebcd79bf9fb76ae0d483523e9a6
senseigithub <adriibasti14@gmail.com>
2023-12-05 11:13:12 +0100

    0.3v

--- Model/Deck.java
+++ Model/Deck.java
@@ -1,5 +1,33 @@
 package Model;
 
+import java.util.Collections;
+
+
 public class Deck {
     //El parking es la baraja.
+    public static Card [] newDeck = new Card[52];
+
+    private static String [] suit = new String[] {"Corazones,Diamantes,Tréboles,Picas"};
+
+    public Deck(int [] Card){
+    int numeroCartas=0;
+     for (int i = 1; i <= 13; i++) {
+        // Añadir cada número cuatro veces a la baraja
+
+        newDeck[numeroCartas]= new Card(i, "Corazones");
+        numeroCartas++;
+        newDeck[numeroCartas]= new Card(i, "Picas");
+        numeroCartas++;
+        newDeck[numeroCartas]= new Card(i, "Diamantes");
+        numeroCartas++;
+        newDeck[numeroCartas]= new Card(i, "Tréboles");
+        numeroCartas++;
+        }
+    }
+
+    /*
+    public void mezclar() {
+        Collections.shuffle(cartas);
+    }
+*/
 }
--- View/Menu.java
+++ View/Menu.java
@@ -2,7 +2,10 @@ package View;
 
 public class Menu {
 
-    public static int menu(){
+    public static int mainMenu(){
+        System.out.println("***********************************");
+        System.out.println("Bienvenidos a mi mesa de BlackJack");
+        System.out.println("***********************************");
 
     }
 }
