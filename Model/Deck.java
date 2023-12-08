c6ddb9d16609fb272918ce79a09c8e6a3878311c
senseigithub <adriibasti14@gmail.com>
2023-12-08 12:51:03 +0100

    0.5v

--- Model/Deck.java
+++ Model/Deck.java
@@ -1,41 +1,46 @@
 package Model;
 
 import java.util.Collections;
+import java.util.List;
 
 
 public class Deck {
     //El parking es la baraja.
-    public static Card [] newDeck = new Card[52];
+    public static Card [] deck = new Card[52]; //Arreglo de cartas con las 52 cartas.
 
-    private static String [] suit = new String[] {"Corazones,Diamantes,Tréboles,Picas"};
+    private static String [] suit = new String[] {"Corazones,Diamantes,Tréboles,Picas"}; //Arreglo de las pintas.
 
+    /**
+     * Este clase lo que hace es recorrer la baraja, y asignar los números cuatro veces, luego les asigna el palo
+     * y los baraja.
+     */
     public Deck(){
     int numeroCartas=0;
      for (int i = 1; i <= 13; i++) {
         // Añadir cada número cuatro veces a la baraja
 
-        newDeck[numeroCartas]= new Card(i, "Corazones");
+        deck[numeroCartas]= new Card(i, "Corazones");
         numeroCartas++;
-        newDeck[numeroCartas]= new Card(i, "Picas");
+        deck[numeroCartas]= new Card(i, "Picas");
         numeroCartas++;
-        newDeck[numeroCartas]= new Card(i, "Diamantes");
+        deck[numeroCartas]= new Card(i, "Diamantes");
         numeroCartas++;
-        newDeck[numeroCartas]= new Card(i, "Tréboles");
+        deck[numeroCartas]= new Card(i, "Tréboles");
         numeroCartas++;
         }
+     mezclar(); //Las baraja despues de crearlas.
     }
 
     @Override
     public String toString() {
-        for (int i= 1;i<= newDeck.length;i++){
-            System.out.println(newDeck[i]);
+        for (int i= 0;i<= deck.length;i++){
+            System.out.println(deck[i]);
         }
         return super.toString();
     }
 
-    /*
+
     public void mezclar() {
-        Collections.shuffle(cartas);
+        Collections.shuffle(List.of(deck));
     }
-*/
 }
