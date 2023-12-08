750dea2e0b7df2b424bbb9c240198ce316bcc162
senseigithub <adriibasti14@gmail.com>
2023-12-09 00:21:49 +0100

    0.7v

--- Controller/MainController.java
+++ Controller/MainController.java
@@ -2,6 +2,44 @@ package Controller;
 
 import View.Menu;
 
+import java.util.Scanner;
+
 public class MainController {
-    Menu p= new Menu();
+    Menu menu= new Menu();
+    int opcion=valid();
+
+    public void run(){
+        menu.mainMenu();
+    }
+
+    public int valid(){
+        //Este metodo va a validar las opciones introducidas por el usuario
+        Scanner teclado = new Scanner(System.in);
+        int opcion=teclado.nextInt();
+        boolean valid = true;
+
+        do {
+            try {
+                opcion = teclado.nextInt();
+                valid = false;
+            } catch (Exception e){
+                System.out.println("Pon los valores correctamente");
+                teclado.nextLine();
+            }
+
+        } while (opcion<1 || opcion>4);
+        return opcion;
+    }
+
+    public void chooseOpcion(int opcion){
+        //Este metodo va a recibir la opcion validada para hacer el switch
+        switch (opcion){
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+        }
+    }
 }
--- View/Menu.java
+++ View/Menu.java
@@ -1,20 +1,12 @@
 package View;
 
 public class Menu {
-    public static int mainMenu(){
-        int opcion=0;
+    public void mainMenu(){
         System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
         System.out.println("Bienvenidos a mi mesa de BlackJack");
         System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
         System.out.println("-Pulse 1 para jugar contra la IA");
         System.out.println("-Pulse 2 para salir del juego");
         System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
-        switch (opcion){
-            case 1:
-
-                break;
         }
-        return 0;
-        }
-
 }
